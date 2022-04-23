af6a2b52ca5db2235f2eaf51e77886ad739cebf6
mohammad <mr.dori.dev@gmail.com>
2022-04-23 22:00:50 +0430

    complete create view

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
+from .models import Url
+from .utils import generate_uuid
 
 
 def index(request: WSGIRequest):
@@ -9,4 +12,11 @@ def index(request: WSGIRequest):
 
 
 def create(request: WSGIRequest):
-    pass
+    """"""
+    if request.method == "POST":
+        link = request.POST['link']
+        uuid = generate_uuid()
+        new_url = Url(link=link, uuid=uuid)
+        new_url.save()
+        return HttpResponse(uuid)
+    return HttpResponse("Nothings Here")
