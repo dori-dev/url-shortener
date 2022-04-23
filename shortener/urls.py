3b53e1fc3acd102a03d7e8234d56f19c5397e7fa
mohammad <mr.dori.dev@gmail.com>
2022-04-23 20:57:38 +0430

    setup index page of url shortener

---
+++ shortener/urls.py
@@ -0,0 +1,8 @@
+"""shortener urls
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index")
+]
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
 
-# Create your views here.
+
+def index(request: WSGIRequest):
+    """index page of url shortener website
+    """
+    return render(request, 'index.html')
